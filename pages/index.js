744f9c3c63772786ff8185816ca121e1369d36d1
pat <patnarin241240@gmail.com>
2019-09-14 00:24:01 +0700

    :alien:page index

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,7 @@ import React from 'react'
 import Nav from '../components/nav'
 import '../styles/base.scss'
 import Sheetapi from '../config/api'
-import Menu_icon from '../components/layout/menu-icon';
-import Carousel from '../components/layout/carousel'
-import Cover from '../components/layout/Cover';
+import Cover from '../components/layout/Cover'
 
 class Home extends React.Component {
 
