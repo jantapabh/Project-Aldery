037f95fe8450f2e3273afc5639b4c24724b61635
pat <patnarin241240@gmail.com>
2019-09-14 00:20:50 +0700

    :alien:page index

--- components/layout/cover.js
+++ components/layout/cover.js
@@ -13,23 +13,70 @@ class Cover extends React.Component {
                 { label: "อายุ70ถึง79ปี", color: "#ffc107" },
                 { label: "อายุ80ถึง89ปี", color: "#00d084" },
                 { label: "อายุมากกว่าหรือเท่ากับ90ปี", color: "#0693e3" }
-            ]
+            ],
+            data:[]
         }
     }
 
+    async componentDidMount() {
+
+        let userOauth = JSON.parse(localStorage.getItem("myOauth"))
+        this.access_token = userOauth.data.access_token
+        await this.list('sheets!D2:L2')
+    }
+
+    list = async (value) => {
+
+        try {
+
+            this.list = await Sheetapi.getSheet2(this.access_token, value)
+            console.log("LIst:", this.list);
+
+
+            for (let i = 0; i < this.list.length; i++) {
+                let value = await {
+                    name: this.list[i][0],
+                    อายุ60ถึง69ปี: parseInt(this.list[i][1].replace(",", "")),
+                    อายุ60ถึง69ปีร้อยละ: parseFloat(this.list[i][2].replace(",", "")),
+                    อายุ70ถึง79ปี: parseInt(this.list[i][3].replace(",", "")),
+                    อายุ70ถึง79ปีร้อยละ: parseFloat(this.list[i][4].replace(",", "")),
+                    อายุ80ถึง89ปี: parseInt(this.list[i][5].replace(",", "")),
+                    อายุ80ถึง89ปีร้อยละ: parseFloat(this.list[i][6].replace(",", "")),
+                    อายุมากกว่าหรือเท่ากับ90ปี: parseInt(this.list[i][7].replace(",", "")),
+                    อายุมากกว่าหรือเท่ากับ90ปีร้อยละ: parseFloat(this.list[i][8].replace(",", "")),
+                }
+                this.setState(prevState => ({
+                    data: [...prevState.data, value.อายุ60ถึง69ปีร้อยละ,value.อายุ70ถึง79ปีร้อยละ,value.อายุ80ถึง89ปีร้อยละ,value. อายุมากกว่าหรือเท่ากับ90ปีร้อยละ],
+                }))
+            }
+        } catch (err) {
+            console.log(err);
+        }
+
+    }
+
     render() {
+
+        console.log(this.state.data);
+        
         return (
             <div className="warp-cover">
-                <img src="/static/bg.jpg" />
+
+
                 <div className="vertical-center">
                     <div className="box">
-                        <h1>Elderly Database</h1>
-                        <h2>hello world</h2>
-                        <div className="box-icon">
-                            <IconData
-                                icon={this.state.icon}
-                            />
+                        <div className="box-content">
+                            <h1>Elderly Database</h1>
+                            <h2>present</h2>
+                            <div className="box-icon">
+                                <IconData
+                                    icon={this.state.icon}
+                                    data={this.state.data}
+                                />
+                            </div>
+
                         </div>
+
                     </div>
                 </div>
             </div>
--- components/layout/icon-data.js
+++ components/layout/icon-data.js
@@ -4,80 +4,95 @@ import React from 'react'
 class IconData extends React.Component {
 
     static defaultProps = {
-        icon: []
+        icon: [],
+        data: []
     }
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            data: [],
-        }
-    }
-
-    async componentDidMount() {
-
-        let userOauth = JSON.parse(localStorage.getItem("myOauth"))
-        this.access_token = userOauth.data.access_token
-        await this.list('sheets!D2:L2')
-    }
-
-    list = async (value) => {
-
-        try {
-
-            this.list = await Sheetapi.getSheet2(this.access_token, value)
-            console.log("LIst:",this.list);
-            
-
-            for (let i = 0; i < this.list.length; i++) {
-                let value = await {
-                    name: this.list[i][0],
-                    อายุ60ถึง69ปี: parseInt(this.list[i][1].replace(",", "")),
-                    อายุ60ถึง69ปีร้อยละ: parseFloat(this.list[i][2].replace(",", "")),
-                    อายุ70ถึง79ปี: parseInt(this.list[i][3].replace(",", "")),
-                    อายุ70ถึง79ปีร้อยละ: parseFloat(this.list[i][4].replace(",", "")),
-                    อายุ80ถึง89ปี: parseInt(this.list[i][5].replace(",", "")),
-                    อายุ80ถึง89ปีร้อยละ: parseFloat(this.list[i][6].replace(",", "")),
-                    อายุมากกว่าหรือเท่ากับ90ปี: parseInt(this.list[i][7].replace(",", "")),
-                    อายุมากกว่าหรือเท่ากับ90ปีร้อยละ: parseFloat(this.list[i][8].replace(",", "")),
-                }
-                this.setState(prevState => ({
-                    data: [...prevState.data, value]
-                }))
-            }
-        } catch (err) {
-            console.log(err);
-        }
-
-    }
 
     render() {
 
-        console.log(this.props.data);
 
         return (
-            <div >
+            <div className="icon-data" >
                 {
                     this.props.icon.map((item, index) => {
                         return (
-                            <React.Fragment>
-                                <svg
-                                    className="progress-ring"
-                                    height="120"
-                                    width="120"
-                                >
-                                    <circle
-                                        className="progress-ring__circle"
-                                        stroke={item.color}
-                                        stroke-width="3"
-                                        fill="transparent"
-                                        r="50"
-                                        cx="60"
-                                        cy="60"
-                                    />
-                                </svg>
-                            </React.Fragment>
+                            <div className="icon-data">
+                                <div className="icon-content">
+                                    <svg
+                                        className="progress-ring"
+                                        height="120"
+                                        width="120"
+                                    >
+                                        <circle
+                                            className="progress-ring__circle"
+                                            stroke={item.color}
+                                            stroke-width="6"
+                                            fill="transparent"
+                                            r="50"
+                                            cx="60"
+                                            cy="60"
+                                        />
+
+                                        {
+                                            this.props.data.map((items, indexs) => {
+
+                                                return (
+                                                    index == 0 && indexs == 0 ?
+                                                        <text key={index}
+                                                            x="60"
+                                                            y="70"
+                                                            text-anchor="middle"
+                                                            stroke-width="1px"
+                                                        >
+                                                            {items}
+                                                        </text>
+                                                        :
+                                                        index == 1 && indexs == 1 ?
+                                                            <text key={index}
+                                                                x="60"
+                                                                y="70"
+                                                                text-anchor="middle"
+                                                                stroke-width="1px"
+                                                            >
+                                                                {items}
+                                                            </text>
+                                                            :
+                                                            index == 2 && indexs == 2 ?
+                                                                <text key={index}
+                                                                    x="60"
+                                                                    y="70"
+                                                                    text-anchor="middle"
+                                                                    stroke-width="1px"
+                                                                >
+                                                                    {items}
+                                                                </text>
+                                                                :
+                                                                index == 3 && indexs == 3 ?
+                                                                    <text key={index}
+                                                                        x="60"
+                                                                        y="70"
+                                                                        text-anchor="middle"
+                                                                        stroke-width="1px"
+                                                                    >
+                                                                        {items}
+                                                                    </text>
+                                                                    :
+                                                                    null
+                                                )
+                                            })
+
+                                        }
+
+
+                                        })
+
+
+                                    </svg>
+                                    <p>{item.label}</p>
+
+                                </div>
+                            </div>
                         )
                     })
                 }
