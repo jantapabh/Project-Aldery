e3d1b7371fa24b0002c64203d35c9a64e40e9b8c
pat <patnarin241240@gmail.com>
2019-09-22 18:41:41 +0700

    reponsive

--- components/chart/radialchart.js
+++ components/chart/radialchart.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import Sheetapi from '../../config/api'
-import { VictoryPie } from "victory";
+import { VictoryChart, VictoryArea } from "victory";
 
 import { LineChart, PieChart } from 'react-chartkick'
 import 'chart.js'
 
+let sampleData = [
+  {
+    value: 50,
+    label: 'Marketing',
+    color: 'red',
+  }, {
+    value: 40,
+    label: 'Sales',
+    color: 'blue'
+  }, {
+    value: 25,
+    label: 'Support',
+    color: 'green'
+  }
 
+]
 
 class Radialchart extends React.Component {
 
@@ -22,29 +37,34 @@ class Radialchart extends React.Component {
 
     let userOauth = JSON.parse(localStorage.getItem("myOauth"))
     this.access_token = userOauth.data.access_token
-    await this.list('!B2:D')
+    await this.list('sheets!D2:L2')
   }
 
   list = async (value) => {
 
     try {
 
-      this.list = await Sheetapi.getSheet1(this.access_token, value)
+      this.list = await Sheetapi.getSheet2(this.access_token, value)
 
       for (let i = 0; i < this.list.length; i++) {
-        let value = await {
+        this.value = await {
           name: this.list[i][0],
-          ชาย60ปีขึ้นไป: parseInt(this.list[i][1].replace(",", "")),
-          หญิง60ปีขึ้นไป: parseInt(this.list[i][2].replace(",", "")), fill: '#ffc658'
+          อายุ60ถึง69ปี: parseInt(this.list[i][1].replace(",", "")),
+          อายุ60ถึง69ปีร้อยละ: parseFloat(this.list[i][2].replace(",", "")),
+          อายุ70ถึง79ปี: parseInt(this.list[i][3].replace(",", "")),
+          อายุ70ถึง79ปีร้อยละ: parseFloat(this.list[i][4].replace(",", "")),
+          อายุ80ถึง89ปี: parseInt(this.list[i][5].replace(",", "")),
+          อายุ80ถึง89ปีร้อยละ: parseFloat(this.list[i][6].replace(",", "")),
+          อายุมากกว่าหรือเท่ากับ90ปี: parseInt(this.list[i][7].replace(",", "")),
+          อายุมากกว่าหรือเท่ากับ90ปีร้อยละ: parseFloat(this.list[i][8].replace(",", "")),
         }
         this.setState(prevState => ({
-          data: [...prevState.data, value]
+          data: [...prevState.data, [{ value }]],
         }))
       }
     } catch (err) {
       console.log(err);
     }
-
   }
 
   render() {
@@ -53,10 +73,12 @@ class Radialchart extends React.Component {
 
     return (
       <div className="warp-chart">
-        
-
-{/* <LineChart data={{"2017-05-13": 2, "2017-05-14": 5,}} /> */}
-<PieChart data={[["Blueberry", 44], ["Strawberry", 23]]} />
+        {/* <VictoryChart
+        >
+          <VictoryArea
+            data={data}
+          />
+        </VictoryChart> */}
       </div>
     )
   }
