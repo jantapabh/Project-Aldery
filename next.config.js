97e2d05ed52c13717d1cf38b10c39065b415f15e
pat <patnarin241240@gmail.com>
2019-10-15 04:29:42 +0700

    :bug:build name not math

--- next.config.js
+++ next.config.js
@@ -1,16 +1,3 @@
-module.exports = {
-  exportPathMap:() => ({
-    '/': {page: '/'},
-    '/main' : {page:'/main'},
-    '/about' : {page:'/about'},
-    '/service' : {page:'/service'},
-    '/mantable' : {page:'/mantable'},
-    '/womantable' : {page:'/womantable'},
-    
-  })
-}
-
-
 const withSass = require('@zeit/next-sass')
 module.exports = withSass({
   /* config options here */
