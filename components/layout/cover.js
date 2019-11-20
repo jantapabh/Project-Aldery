fc4faeb6d32bfcb24738e03d3f12f93c205ff61e
pat <patnarin241240@gmail.com>
2019-11-20 17:22:27 +0700

    cover

--- components/layout/cover.js
+++ components/layout/cover.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 
 class Cover extends React.Component {
 
@@ -7,21 +8,18 @@ class Cover extends React.Component {
 
         return (
             <div className="warp-header">
+                <img src="/static/bg.jpg" alt="cover" />
 
-                {/* <div className="page-content">
+                <div className="page-content">
                     <div className="container-fluid">
-                        <h1>Eldery DB</h1>
-                        <h2>ฐานข้อมูลผู้สูงอายุ และอินโฟกราฟิก</h2>
-                        <div className="textarea-center">
-                            
-                            <h6>เป็นการนำข้อมูลของประชากรผู้ที่เป็นผู้สูงอายุ มาทำอินโฟกราฟิกในรูปแบบต่าง ๆ ซึ่งตัวของเว็บไซต์จะมีการใช้กราฟ<br />
-                                เพื่ออ้างอิงข้อมูลในการแสดงค่าทางสถิติว่าพื้นที่ในส่วนใด ที่มีจำนวนประชากรเป็นผู้สูงอายุมากที่สุด และพื้นที่ใดมีจำนวนน้อยที่สุด<br />
-                                นอกจากนี้ยังมีการให้ข้อมูล เกี่ยวกับสิทธิที่พึ่งได้ของผู้สูงอายุ
-                                </h6>
+                        <h1 className = "animated fadeInDown delay-s">Eldery DB</h1>
+                        <h2 className = "animated fadeInDown delay-s">ฐานข้อมูลผู้สูงอายุ และอินโฟกราฟิก</h2>
+                        <Link href="/main">
+                            <button type="button" class="btn btn-primary animated fadeInDown delay-s">เข้าสู่หน้าหลัก</button>
+                        </Link>
 
-                        </div>
                     </div>
-                </div> */}
+                </div>
 
             </div>
         )
--- pages/index.js
+++ pages/index.js
@@ -64,38 +64,6 @@ class Home extends React.Component {
         <Nav_index name="/" />
         <main>
           <Cover />
-
-          {/* <div className="container-fluid">
-
-                <Map
-                  className={'map'}
-                  google={this.props.google}
-                  zoom={12}
-                  style={mapStyles}
-                  initialCenter={center}
-                >
-                  <Marker
-                    onClick={this.onMarkerClick}
-                    name={'สำนักงานเทศบาลเมืองกะทู้ ตำบล กะทู้ อำเภอกะทู้ ภูเก็ต'}
-                    position={{ lat: 7.9111378, lng: 98.3452833 }}
-                  />
-
-                  <InfoWindow
-                    marker={this.state.activeMarker}
-                    visible={this.state.showingInfoWindow}
-                  >
-                    <div>
-                      <a href="https://www.google.co.th/maps/place/%E0%B8%AA%E0%B8%B3%E0%B8%99%E0%B8%B1%E0%B8%81%E0%B8%87%E0%B8%B2%E0%B8%99%E0%B9%80%E0%B8%97%E0%B8%A8%E0%B8%9A%E0%B8%B2%E0%B8%A5%E0%B9%80%E0%B8%A1%E0%B8%B7%E0%B8%AD%E0%B8%87%E0%B8%81%E0%B8%B0%E0%B8%97%E0%B8%B9%E0%B9%89/@7.9111431,98.3452833,17z/data=!3m1!4b1!4m5!3m4!1s0x3050305d21d9042d:0x24ca59f8b15d6fde!8m2!3d7.9111378!4d98.347472?hl=th">
-                        <h6 >{this.state.selectedPlace.name}</h6>
-                      </a>
-
-                    </div>
-                  </InfoWindow>
-                </Map>
-
-              </div> */}
-
-          {/* <Footer /> */}
         </main>
       </div>
     )
