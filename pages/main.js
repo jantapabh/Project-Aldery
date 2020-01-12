ade9de178c9cee37d3c457706fce757f21e76fb6
pat <patnarin241240@gmail.com>
2020-01-13 01:32:03 +0700

    :recycle:Main

--- pages/main.js
+++ pages/main.js
@@ -9,7 +9,6 @@ const BarMap = dynamic(
     { ssr: false }
 )
 
-
 const Main = () => {
 
     const [status, setStatus] = useState(true)
@@ -44,49 +43,3 @@ const Main = () => {
     )
 }
 export default Main;
-
-// class Main extends React.Component {
-
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             status: true,
-//         };
-//     }
-
-//     onConfirm = (order) => {
-//         this.setState({
-//             status: order,
-//         })
-//     }
-
-//     render() {
-
-//         return (
-//                 <div className="warp-main">
-//                     <Navbar_main confirm={this.onConfirm} status={this.state.status} />
-//                     <Sidebar status={this.state.status} />
-//                     <div className={`wrapper${this.state.status ? " menuDisplayed" : ""}`}>
-//                         <div className="page-content-wrapper">
-//                             <div className="container-fluid">
-//                                 <h1 className="text-center">สังคมผู้สูงอายุ</h1>
-//                                 <h2 className="small text-center"></h2>
-//                                 <div className="warp-map">
-//                                     <div className="map-Img">
-//                                         <Map />
-//                                     </div>
-
-//                                     <div className="map-content">
-//                                         <BarMap />
-//                                     </div>
-
-//                                 </div>
-//                             </div>
-//                         </div>
-//                     </div>
-//             </div>
-
-//         )
-//     }
-// }
-// export default Main;
