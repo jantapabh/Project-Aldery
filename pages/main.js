217c5655e3da47a047662978f3536a3e390f5e17
pat <patnarin241240@gmail.com>
2020-01-13 01:28:33 +0700

    :recycle:Main

--- pages/main.js
+++ pages/main.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import dynamic from 'next/dynamic'
 import Map from '../components/layout/Map';
-import Navbar_main from '../components/navbar_main';
+import Navbar_main from '../components/nav_main';
 import Sidebar from '../components/layout/sidebar';
 
 const BarMap = dynamic(
@@ -10,48 +10,83 @@ const BarMap = dynamic(
 )
 
 
-class Main extends React.Component {
+const Main = () => {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            status: true,
-        };
+    const [status, setStatus] = useState(true)
+
+    const onConfirm = (order) => {
+        setStatus(order)
     }
 
-    onConfirm = (order) => {
-        this.setState({
-            status: order,
-        })
-    }
-
-    render() {
-
-        return (
-                <div className="warp-main">
-                    <Navbar_main confirm={this.onConfirm} status={this.state.status} />
-                    <Sidebar status={this.state.status} />
-                    <div className={`wrapper${this.state.status ? " menuDisplayed" : ""}`}>
-                        <div className="page-content-wrapper">
-                            <div className="container-fluid">
-                                <h1 className="text-center">สังคมผู้สูงอายุ</h1>
-                                <h2 className="small text-center"></h2>
-                                <div className="warp-map">
-                                    <div className="map-Img">
-                                        <Map />
-                                    </div>
-
-                                    <div className="map-content">
-                                        <BarMap />
-                                    </div>
-
-                                </div>
+    return (
+        <div className="warp-main">
+            <Navbar_main confirm={onConfirm} status={status} />
+            <Sidebar status={status} />
+            <div className={`wrapper${status ? " menuDisplayed" : ""}`}>
+                <div className="page-content-wrapper">
+                    <div className="container-fluid">
+                        <h1 className="text-center">สังคมผู้สูงอายุ</h1>
+                        <h2 className="small text-center"></h2>
+                        <div className="warp-map">
+                            <div className="map-Img">
+                                <Map />
                             </div>
+
+                            <div className="map-content">
+                                <BarMap />
+                            </div>
+
                         </div>
                     </div>
+                </div>
             </div>
-
-        )
-    }
+        </div>
+    )
 }
 export default Main;
+
+// class Main extends React.Component {
+
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             status: true,
+//         };
+//     }
+
+//     onConfirm = (order) => {
+//         this.setState({
+//             status: order,
+//         })
+//     }
+
+//     render() {
+
+//         return (
+//                 <div className="warp-main">
+//                     <Navbar_main confirm={this.onConfirm} status={this.state.status} />
+//                     <Sidebar status={this.state.status} />
+//                     <div className={`wrapper${this.state.status ? " menuDisplayed" : ""}`}>
+//                         <div className="page-content-wrapper">
+//                             <div className="container-fluid">
+//                                 <h1 className="text-center">สังคมผู้สูงอายุ</h1>
+//                                 <h2 className="small text-center"></h2>
+//                                 <div className="warp-map">
+//                                     <div className="map-Img">
+//                                         <Map />
+//                                     </div>
+
+//                                     <div className="map-content">
+//                                         <BarMap />
+//                                     </div>
+
+//                                 </div>
+//                             </div>
+//                         </div>
+//                     </div>
+//             </div>
+
+//         )
+//     }
+// }
+// export default Main;
