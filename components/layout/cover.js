b62d1e4e5e2f266907ac0abbefa56485653a47fb
pat <patnarin241240@gmail.com>
2020-01-14 01:20:37 +0700

    :lipstick:Cover

--- components/layout/cover.js
+++ components/layout/cover.js
@@ -4,12 +4,9 @@ import Link from 'next/link'
 class Cover extends React.Component {
 
     render() {
-
-
         return (
             <div className="warp-header">
                 <img src="/static/bg-test4.jpg" alt="cover" />
-
                 <div className="page-content">
                     <div className="container-fluid">
                         <h1 className="animated fadeInDown delay-s">Eldery DB</h1>
@@ -17,10 +14,8 @@ class Cover extends React.Component {
                         <Link href="/main">
                             <button type="button" className="btn btn-primary animated fadeInDown delay-s">เข้าสู่หน้าหลัก</button>
                         </Link>
-
                     </div>
                 </div>
-
             </div>
         )
     }
