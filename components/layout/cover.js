84bc32b7032c1b775d0f9a80d008f26b23802eea
pat <patnarin241240@gmail.com>
2020-03-02 05:46:10 +0700

    :lipstick:Cover

--- components/layout/cover.js
+++ components/layout/cover.js
@@ -6,14 +6,29 @@ class Cover extends React.Component {
     render() {
         return (
             <div className="warp-header">
-                <img src="/static/bg-test4.jpg" alt="cover" />
                 <div className="page-content">
                     <div className="container-fluid">
-                        <h1 className="animated fadeInDown delay-s">Eldery DB</h1>
-                        <h2 className="animated fadeInDown delay-s">ฐานข้อมูลผู้สูงอายุ และอินโฟกราฟิก</h2>
-                        <Link href="/main">
-                            <button type="button" className="btn btn-primary animated fadeInDown delay-s">เข้าสู่หน้าหลัก</button>
-                        </Link>
+                        <div className="title">
+                            <h1 className="animated fadeInDown delay-s">Eldery DataBase </h1>
+                            <h1 className="animated fadeInDown delay-s">& Infographics</h1>
+                            <div className="detail">
+                                <svg >
+                                    <line x1="0" y1="0" x2="0" y2="100" />
+                                </svg>
+                                <div className="text-content">
+                                    <h6>ElderlyDB เป็นระบบฐานข้อมูลผู้สูงอายุ</h6>
+                                    <h6>ซึ่งแสดงผลในรูปเเบบของกราฟในรูปแบบต่าง ๆ </h6>
+                                </div>
+
+                            </div>
+
+                            <Link href="/main">
+                                <h5>เข้าสู่ระบบ</h5>
+                            </Link>
+                        </div>
+                        <div className="cover">
+                            <img src="../../static/project2.png" />
+                        </div>
                     </div>
                 </div>
             </div>
