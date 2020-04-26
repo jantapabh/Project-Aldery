f5f92d81dfd06e8781a091c0d9ba7249b1396b10
pat <patnarin241240@gmail.com>
2020-04-27 01:10:41 +0700

    :lipstick:empty

--- components/Empty.js
+++ components/Empty.js
@@ -3,17 +3,23 @@ import Link from 'next/link';
 
 const Empty = () => {
     return (
-        <div className="warp-empty">
-            <div className="content-empty">
-                <img src="/static/empty.jpg" alt="empty" />
-                <div className="title-empty">
-                    <h4>หน้ายังไม่พร้อมใช้งาน โปรดกลับสู่หน้าหลัก</h4>
-                </div>
-            </div>
-            <Link href="/">
-                <h5>หน้าหลัก</h5>
-            </Link>
-        </div>
+        <React.Fragment>
+            {
+                typeof document === 'undefined' ?
+                    null :
+                        <div className="warp-empty">
+                            <div className="content-empty">
+                                <img src="/static/empty.jpg" alt="empty" />
+                                <div className="title-empty">
+                                    <h4>หน้ายังไม่พร้อมใช้งาน โปรดกลับสู่หน้าหลัก</h4>
+                                </div>
+                            </div>
+                            <Link href="/">
+                                <h5>หน้าหลัก</h5>
+                            </Link>
+                        </div>
+            }
+        </React.Fragment>
     )
 }
 export default Empty;
--- pages/main.js
+++ pages/main.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import Dashboard from '../components/layout/dashboard';
 
 const MainPage = () => {
