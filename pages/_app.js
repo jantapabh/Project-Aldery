5d1b0360b1b8299341203d4b9241ba701516f2a1
pat <patnarin241240@gmail.com>
2020-04-29 00:20:52 +0700

    :sparkles:redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
-import React from 'react'
-import '../styles/base.scss'
-import 'antd/dist/antd.less'
+import React from 'react';
+import '../styles/base.scss';
+import 'antd/dist/antd.less';
+import withReduxStore from '../redux/lib/with-redux-store';
+import { Provider } from "react-redux";
 
 // This default export is required in a new `pages/_app.js` file.
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps, reduxStore }) {
+  return (
+    <Provider store={reduxStore}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
+export default withReduxStore(MyApp)
